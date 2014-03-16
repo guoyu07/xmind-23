1016f91f4f4d0809f3aadbdb74b89adf71e4a075
lejingw <lejingw@163.com>
2014-03-16 14:52:53 +0800

    1

--- pack.sh
+++ pack.sh
@@ -2,15 +2,12 @@
 
 echo "pack xmind file:$1/$2"
 
-cd "$1"
-
+cd "$1/$2"
 zip -r -m "$2_tmp.xmind" ./*
 #-x "$2.xmind"
 #-x pack.sh -x unpack.sh
-
 #rm "$2.xmind"
+mv "$2_tmp.xmind" "../$2.xmind"
 
-mv "$2_tmp.xmind" "$2.xmind"
-
-cd ..
+cd ../..
 
--- unpack.sh
+++ unpack.sh
@@ -3,9 +3,7 @@
 echo "unpack xmind file: $1/$2"
 
 cd "$1"
-
-unzip "$2.xmind"
-
+mkdir $2
+unzip "$2.xmind" -d $2
 rm "$2.xmind"
-
 cd ..
